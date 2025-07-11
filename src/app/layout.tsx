f6b4d840a26f67205482ff84719d7ab6ee11c37d
Aafiya Khan <khanaafiya4747@gmail.com>
2025-07-11 03:53:14 +0000

    put this code in head tag of main html file -> <script async src="https:

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type {Metadata} from 'next';
+import Script from 'next/script';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 
@@ -19,6 +20,8 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
+        <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4573761203080537"
+     crossOrigin="anonymous"></Script>
       </head>
       <body className="font-body antialiased">
         {children}
